1dc664c44d7be68116034e85b6111359110e65a7
dw2561103 <duwei1983du@163.com>
2016-04-25 18:38:31 +0800

    ceshi2

---
+++ src/com/example/asyncimageloader/Images.java
@@ -0,0 +1,10 @@
+package com.example.asyncimageloader;
+
+public class Images {
+
+	public final static String[] imageThumbUrls = new String[] {
+		"http://g.hiphotos.baidu.com/image/pic/item/f3d3572c11dfa9ecb40b877665d0f703918fc1ae.jpg", 
+		"http://b.hiphotos.baidu.com/image/pic/item/09fa513d269759ee5c8f9e88b5fb43166c22df92.jpg",
+		"http://g.hiphotos.baidu.com/imgad/pic/item/023b5bb5c9ea15cec0e68e76b1003af33a87b241.jpg"	, };
+	
+}
